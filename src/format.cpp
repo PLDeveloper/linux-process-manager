42869b6c1fbbbf296d55d39abfc53339023148bf
Ben Trybulski <btrybulski1@gmail.com>
2020-04-09 06:15:28 -0400

    Implemented ElapsedTime for displaying process uptime as a formatted string

--- src/format.cpp
+++ src/format.cpp
@@ -1,11 +1,32 @@
 #include <string>
-
+#include <stdio.h>
 #include "format.h"
 
 using std::string;
 
-// TODO: Complete this helper function
-// INPUT: Long int measuring seconds
-// OUTPUT: HH:MM:SS
-// REMOVE: [[maybe_unused]] once you define the function
-string Format::ElapsedTime(long seconds[[maybe_unused]]) { return string(); }
+// Input: 
+//  Total number of seconds
+// Output: 
+//  HH:MM:SS
+// Notes: 
+//  Using integer division 
+//    seconds = 3600 * HH + 60 * MM + SS 
+//    -> seconds / 3600 = HH 
+//    -> seconds % 3600 = 60 * MM + SS 
+
+string Format::ElapsedTime(long seconds) { 
+  int h, m, s;
+
+  h = seconds / 3600; 
+  seconds = seconds % 3600;
+
+  m = seconds / 60;
+  seconds = seconds % 60; 
+
+  s = seconds;
+
+  char buf[10];
+  sprintf(buf, "%02d:%02d:%02d", h, m, s);
+
+  return string(buf); 
+}
