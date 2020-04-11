9f185e62149328acab6cbd77a3222f1fdc004c8c
Ben Trybulski <btrybulski1@gmail.com>
2020-04-11 10:58:47 -0400

    Can compute aggregate cpu utilization

--- src/processor.cpp
+++ src/processor.cpp
@@ -1,4 +1,33 @@
+#include <cassert>
+#include <string>
 #include "processor.h"
+#include "linux_parser.h"
 
-// TODO: Return the aggregate CPU utilization
-float Processor::Utilization() { return 0.0; }
+using std::string;
+using std::stof;
+
+// Returns the aggregate CPU utilization (i.e. summed across cores)
+float Processor::Utilization() { 
+  auto values = LinuxParser::CpuUtilization(); 
+  for (string v : values) {
+    if (v.empty()) {
+      v = "0";
+    }
+  }
+
+  float user, nice, system, idle, iowait, irq, softirq, steal;
+  user = stof(values[0]); 
+  nice = stof(values[1]); 
+  system = stof(values[2]);
+  idle = stof(values[3]);
+  iowait = stof(values[4]);
+  irq = stof(values[5]);
+  softirq = stof(values[6]);
+  steal = stof(values[7]);
+
+  float idle_time = idle + iowait;
+  float non_idle_time = user + nice + system + irq + softirq + steal;
+  float total = idle_time + non_idle_time;
+  assert(total != 0);
+  return non_idle_time / total;
+}
